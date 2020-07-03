27e5b083a5af7ec54d5edb62fa0fa58f5e9986c4
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-02 21:22:56 -0600

    borrarlo en caso de encontrar error

--- sistema_mrp/sistema_mrp/vistas/Administracion/AddProducto.cs
+++ sistema_mrp/sistema_mrp/vistas/Administracion/AddProducto.cs
@@ -107,16 +107,6 @@ namespace sistema_mrp.vistas.Administracion
 
         private void tbNombre_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
-            {
-                e.Handled = true;
-            }
-
-            // solo 1 punto decimal
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-            {
-                e.Handled = true;
-            }
         }
 
         private void tbDescripcion_KeyPress(object sender, KeyPressEventArgs e)
